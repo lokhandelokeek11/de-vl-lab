aeb0a9767ed8776b85d1754348cd9ef150763634
Lokeek Lokhande <lokeeklokhande1185@gmail.com>
2024-08-20 14:33:40 +0530

    Create array_slicing2.py

---
+++ Num Py/array_slicing2.py
@@ -0,0 +1,10 @@
+#Slicing arrays 
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])  #[start:end]. [start:end:step]
+print(arr[1:5])
+
+# Slice elements from index 4 to the end of the array:
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[4:])
